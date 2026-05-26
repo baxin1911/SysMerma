14d1d61ab8bb6d878065773634dd85433a31f7ca
Alberto Galindo <albertogab1c@gmail.com>
2026-05-25 19:48:50 -0600

    Ajusta filtros de asesores para clientes no internos

--- public/js/plugins/select2/modules/goodsIssueSelect.js
+++ public/js/plugins/select2/modules/goodsIssueSelect.js
@@ -1,5 +1,5 @@
 import { getSelectedOptionText } from "../../../utils/domUtils.js";
-import { resolveAdvisorDepartmentByClientName, isInternalClientName, resolveProjectNumberByClientAndDepartment, salesDepartmentName } from "../../../application/warehouse/goodsIssues/goodsIssueRules.js";
+import { resolveAdvisorDepartmentByClientName, isInternalClientName, resolveProjectNumberByClientAndDepartment } from "../../../application/warehouse/goodsIssues/goodsIssueRules.js";
 import { toggleDisabledElement } from "../../../utils/formUtils.js";
 import { bindDependency } from "../baseSelect.js";
 import { setupClientSelect, toggleClientOption } from "../domains/client.js";
@@ -64,8 +64,7 @@ export const initGoodsIssueFormSelect2 = () => {
             return {
                 search: params.term,
                 department: resolveAdvisorDepartmentByClientName({
-                    clientName: getSelectedOptionText(clientScopedSelector),
-                    fallbackDepartment: salesDepartmentName
+                    clientName: getSelectedOptionText(clientScopedSelector)
                 }),
                 strictDepartmentFilter: true
             };
